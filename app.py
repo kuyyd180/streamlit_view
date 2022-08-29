348338221bc6db846faf084225cf5ae2dc860758
DallHyun <99791281+kuyyd180@users.noreply.github.com>
2022-08-29 17:24:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 print('Hello!')
+
+view = [100, 150, 30]
+st.write('# view')
+st.write('## raw')
+view
+st.write('## bar chart')
+st.bar_chart(view)
+import pandas as pd
+sview = pd.Series(view)
+sview
